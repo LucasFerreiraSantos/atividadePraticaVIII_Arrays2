a22fc5ac0fdab6cc03b3456651a927324f093bb3
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-31 19:56:39 -0300

    feito o exer2

--- exer2/index.js
+++ exer2/index.js
@@ -1,3 +1,13 @@
+const numInteiros = []
+let contador = 1
+let soma = 0
+  for(let i = 0; i < 20; i++){
+    numInteiros.push(contador)
+    soma += contador
+    contador++
+  }
+  document.write(numInteiros)
+  document.write(`<br>A soma dos elementos do Array é ${soma}.`)
 
 // 2. Desenvolva um algoritmo para percorrer um vetor de 20 elementos
 // inteiros e no final mostre a soma de todos os elementos.
