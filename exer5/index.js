fa17317fa66414ab08fead842a84742d04f0644d
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-31 22:16:49 -0300

    feito o exer5

--- exer5/index.js
+++ exer5/index.js
@@ -1,4 +1,11 @@
+function verificarNumeroInt(){
+  const numInteiros = [1, 2, 3, 4, 5, 6, 7]
+  const num = 3
+  if(numInteiros.indexOf(num)) return 'VERDADEIRO!';
+  else return 'FALSO'
+}
 
+document.write(verificarNumeroInt())
 
 // 5. Crie uma função que recebe um vetor de inteiros e um número
 // inteiro e retorne verdadeiro se o número faz parte do vetor ou falso
