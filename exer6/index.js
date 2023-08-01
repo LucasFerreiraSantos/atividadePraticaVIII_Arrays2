469739bcccb30a7d37ffa00b48f33714a283c76f
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-31 22:33:47 -0300

    feito o exer6 e concluido a lista

--- exer6/index.js
+++ exer6/index.js
@@ -1,4 +1,11 @@
+const nomes = []
+while(nomes.length < 5){
+  const entrada = prompt('Digite um nome: ')
+  nomes.push(entrada)
+}
 
+document.write(nomes.join(', '))
+document.write(`<p>${nomes.reverse()}</p>`)
 
 // 6. Escreva um algoritmo que solicite ao usuário a entrada de 5 nomes,
 // e que exiba a lista desses nomes na tela. Após exibir essa lista, o
