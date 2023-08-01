e94b3356036d0f349574e91467004b02ca2dff6c
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-31 21:16:48 -0300

    feito o exer3

--- exer3/index.js
+++ exer3/index.js
@@ -1,4 +1,31 @@
+const numerosPerfeitos = []
+let limite = 4
+let num = 2
 
+function verificarNumPrimo(num){
+  let divisores = 0
+
+  for(let i=1; i<=num; i++)
+    if(num % i === 0) divisores++
+
+    if(divisores==2){
+      numPrimo = true
+      return numPrimo
+    }
+    else{
+      numPrimo = false
+    }
+}
+
+while (numerosPerfeitos.length < limite) {
+  if (verificarNumPrimo(num)){
+    const perfeito = (2 ** (num - 1)) * (2 ** num - 1)
+    numerosPerfeitos.push(perfeito)
+  }
+  num++
+}
+
+document.write(`${numerosPerfeitos.join(' ')}`)
 
 // 3. Desenvolva um algoritmo que preenche um vetor com os 4
 // primeiros números perfeitos.
